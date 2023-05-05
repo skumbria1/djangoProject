4e488cd77620a84747b88560c77a41580aa5d650
Anton Laptev <anton.laptev.p@gmail.com>
2023-05-05 13:20:47 +0300

    Done HW 7

--- mysite/core/admin.py
+++ mysite/core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Actor)
+class Actor(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'second_name', 'gender', 'actorid',
+                    'creativeteam']
+    ordering = ['id']
+
+
+@admin.register(models.ActorId)
+class ActorId(admin.ModelAdmin):
+    list_display = ['number', 'photo']
+    ordering = ['number']
+
+
+@admin.register(models.CreativeTeam)
+class CreativeTeam(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    ordering = ['id']
+
+
+@admin.register(models.Play)
+class Play(admin.ModelAdmin):
+    list_display = ('id', 'title', 'premiere_date')
+    ordering = ['id']
--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class CreativeTeam(models.Model):
+    name = models.CharField('Название группы', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Play(models.Model):
+    title = models.CharField('Название', max_length=255)
+    premiere_date = models.DateField('Дата премьеры')
+
+    def __str__(self):
+        return self.title
+
+
+class ActorId(models.Model):
+    number = models.CharField('Номер', max_length=255, unique=True,
+                              primary_key=True)
+    photo = models.CharField('Фото', max_length=255, null=True)
+
+    def __str__(self):
+        return self.number
+
+
+class Actor(models.Model):
+    GENDER_CHOICE = (
+        ('M', 'Мужской'),
+        ('F', 'Женский'),
+    )
+    first_name = models.CharField('Имя', max_length=255)
+    second_name = models.CharField('Фамилия', max_length=255)
+    gender = models.CharField('Пол', max_length=255, choices=GENDER_CHOICE,
+                              default=GENDER_CHOICE[0])
+    actorid = models.OneToOneField(ActorId, on_delete=models.CASCADE, null=True)
+    creativeteam = models.ForeignKey(CreativeTeam, on_delete=models.CASCADE,
+                                     null=True, related_name='actors')
+    plays = models.ManyToManyField(Play, blank=True, related_name='actors')
+
+    def __str__(self):
+        return f'{self.first_name} {self.second_name}'
