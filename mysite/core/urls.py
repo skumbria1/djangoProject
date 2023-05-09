9e4a802b40bc0d03768eb1c57ed23765e02d7e2d
Anton Laptev <anton.laptev.p@gmail.com>
2023-05-09 15:54:32 +0300

    Done HW 8

--- mysite/core/urls.py
+++ mysite/core/urls.py
@@ -19,13 +19,36 @@ from django.urls import path
 from django.views.generic import RedirectView
 from . import views
 
+
+app_name = 'core'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/employees/')),
-    path('hello/', views.hello_world),
-    path('employees/', views.employees, name='employees'),
-    path('employee_of_the_month/',
-         views.employee_of_the_month,
-         name='employee_of_the_month'),
-    path('add_employee/', views.add_employee, name='add_employee')
+    path('', RedirectView.as_view(url='/actors/')),
+    path('actors/', views.ActorList.as_view(), name='actors'),
+    path('actors_f/', views.get_actor_list, name='actors_f'),
+    path(
+        'actor_detail/<int:pk>',
+        views.ActorDetail.as_view(),
+        name='actor_detail'
+    ),
+    path(
+        'actor_detail_f/<int:pk>',
+        views.get_actor_detail,
+        name='actor_detail_f'
+    ),
+    path('actor_ids/', views.ActorIdList.as_view(), name='actor_ids'),
+    path('actor_ids_f/', views.get_actor_id_list, name='actor_ids_f'),
+    path(
+        'creative_teams/',
+        views.CreativeTeamList.as_view(),
+        name='creative_teams'
+    ),
+    path(
+        'creative_teams_f/',
+        views.get_creative_team_list,
+        name='creative_teams_f'
+    ),
+    path('plays/', views.PlayList.as_view(), name='plays'),
+    path('plays_f/', views.get_play_list, name='plays_f'),
 ]
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,18 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from . import models
 
 
-def hello_world(request):
-    return HttpResponse(content='hello world!')
+class ActorList(ListView):
+    model = models.Actor
+    template_name = 'core/actors.html'
+    context_object_name = 'actors'
 
 
-def employees(request):
-    return render(request, 'core/employees.html')
+def get_actor_list(request):
+    actors = models.Actor.objects.all()
+    context = {'actors': actors}
+    return render(
+        request=request,
+        template_name='core/actors.html',
+        context=context
+    )
 
 
-def employee_of_the_month(request):
-    return render(request, 'core/employee_of_the_month.html')
+class ActorDetail(DetailView):
+    model = models.Actor
+    template_name = 'core/actor_detail.html'
+    context_object_name = 'actor'
 
 
-def add_employee(request):
-    return render(request, 'core/add_employee.html')
+def get_actor_detail(request, pk):
+    actor = get_object_or_404(models.Actor, pk=pk)
+    context = {'actor': actor}
+    return render(
+        request=request,
+        template_name='core/actor_detail.html',
+        context=context
+    )
+
+
+class ActorIdList(ListView):
+    model = models.ActorId
+    template_name = 'core/actor_ids.html'
+    context_object_name = 'actor_ids'
+
+
+def get_actor_id_list(request):
+    actor_ids = models.ActorId.objects.all()
+    context = {'actor_ids': actor_ids}
+    return render(
+        request=request,
+        template_name='core/actor_ids.html',
+        context=context
+    )
+
+
+class PlayList(ListView):
+    model = models.Play
+    template_name = 'core/plays.html'
+    context_object_name = 'plays'
+
+
+def get_play_list(request):
+    plays = models.Play.objects.all()
+    context = {'plays': plays}
+    return render(
+        request=request,
+        template_name='core/plays.html',
+        context=context
+    )
+
+
+class CreativeTeamList(ListView):
+    model = models.CreativeTeam
+    template_name = 'core/creative_teams.html'
+    context_object_name = 'creative_teams'
+
+
+def get_creative_team_list(request):
+    creative_teams = models.CreativeTeam.objects.all()
+    context = {'creative_teams': creative_teams}
+    return render(
+        request=request,
+        template_name='core/creative_teams.html',
+        context=context
+    )
